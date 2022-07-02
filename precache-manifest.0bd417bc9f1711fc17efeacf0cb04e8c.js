d408630865de9636f008472719ad05dc48c5322e
Tareq Mahmud <tareqmahmud@Tareqs-MacBook-Air.local>
2022-07-02 16:00:44 +0600

    Updates

---
+++ precache-manifest.0bd417bc9f1711fc17efeacf0cb04e8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb064ad075b8a7e5e300",
+    "url": "/country-directory/static/css/main.729cb506.chunk.css"
+  },
+  {
+    "revision": "cb064ad075b8a7e5e300",
+    "url": "/country-directory/static/js/main.cb064ad0.chunk.js"
+  },
+  {
+    "revision": "64de04372cf6dda1ff22",
+    "url": "/country-directory/static/js/1.64de0437.chunk.js"
+  },
+  {
+    "revision": "224482847e8ce3aeab15",
+    "url": "/country-directory/static/js/runtime~main.22448284.js"
+  },
+  {
+    "revision": "fd7c2888aaafe6914b700be750ae14f3",
+    "url": "/country-directory/index.html"
+  }
+];
